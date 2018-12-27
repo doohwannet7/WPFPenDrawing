2e18a843e70ece2c0b9f44e20dda0ca8f06ada9e
dhsung <dhsung@DESKTOP-0GADPPR>
2018-12-27 22:09:02 +0900

    2018.12.27 InkCanvas로 갈아타야됨...

--- WPFPenDrawing/WPFPenDrawing/MainWindow.xaml.cs
+++ WPFPenDrawing/WPFPenDrawing/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,84 @@ namespace WPFPenDrawing
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new ViewModel.MainViewModel();
+
+            //Image1.Source = (new ImageSourceConverter()).ConvertFromString("pack://application:,,,/Resources/kubota.png") as ImageSource;
+            //Image1.Source = new BitmapImage(WPFPenDrawing.Properties.Resources.kubota);
+            //image1.Source = new BitmapImage(new Uri(@"pack://application:,,,/Image/kubota.PNG", UriKind.Absolute));
+
+        }
+        System.Windows.Point currentPoint = new System.Windows.Point();
+
+        private void Canvas1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                currentPoint = e.GetPosition(this);
+        }
+
+        private void Canvas1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                //SolidColorBrush blackBrush = new SolidColorBrush();
+                //blackBrush.Color = Colors.Black;
+
+                //Polyline yellowPolyline = new Polyline();
+                //yellowPolyline.Stroke = blackBrush;
+                //yellowPolyline.StrokeThickness = 4;
+
+                //yellowPolyline.Points.Add(currentPoint);
+                //yellowPolyline.Points.Add(e.GetPosition(this));
+                //canvas1.Children.Add(yellowPolyline);
+
+                Line line = new Line();
+                line.Stroke = System.Windows.SystemColors.WindowFrameBrush;
+                line.StrokeThickness = 10;
+                line.StrokeLineJoin = PenLineJoin.Round;
+                line.StrokeStartLineCap = PenLineCap.Round;
+                line.StrokeEndLineCap = PenLineCap.Round;
+
+
+                line.X1 = currentPoint.X;
+                line.Y1 = currentPoint.Y;
+                line.X2 = e.GetPosition(this).X;
+                line.Y2 = e.GetPosition(this).Y;
+
+                currentPoint = e.GetPosition(this);
+
+                //canvas1.Children.Add(line);
+            }
+        }
+
+
+        private void CreateAPolyline()
+        {
+            // Create a blue and a black Brush  
+            SolidColorBrush yellowBrush = new SolidColorBrush();
+            yellowBrush.Color = Colors.Yellow;
+            SolidColorBrush blackBrush = new SolidColorBrush();
+            blackBrush.Color = Colors.Black;
+            // Create a polyline  
+            Polyline yellowPolyline = new Polyline();
+            yellowPolyline.Stroke = blackBrush;
+            yellowPolyline.StrokeThickness = 4;
+            // Create a collection of points for a polyline  
+            System.Windows.Point Point1 = new System.Windows.Point(10, 100);
+            System.Windows.Point Point2 = new System.Windows.Point(100, 200);
+            System.Windows.Point Point3 = new System.Windows.Point(200, 30);
+            System.Windows.Point Point4 = new System.Windows.Point(250, 200);
+            System.Windows.Point Point5 = new System.Windows.Point(200, 150);
+            PointCollection polygonPoints = new PointCollection();
+            polygonPoints.Add(Point1);
+            polygonPoints.Add(Point2);
+            polygonPoints.Add(Point3);
+            polygonPoints.Add(Point4);
+            polygonPoints.Add(Point5);
+            // Set Polyline.Points properties  
+            yellowPolyline.Points = polygonPoints;
+            // Add polyline to the page  
+            //canvas1.Children.Add(yellowPolyline);
         }
     }
 }
---
+++ WPFPenDrawing/WPFPenDrawing/ViewModel/MainViewModel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFPenDrawing.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private int number;
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                int old = number;
+                if(value > 100)
+                {
+                    System.Windows.MessageBox.Show("100 초과");
+                    number = old;
+                    OnPropertyChanged("Number");
+                
+                }
+                else
+                {
+                    number = value;
+                    OnPropertyChanged("Number");
+                }
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private ICommand testCommand;
+        public ICommand TestCommand
+        {
+            get
+            {
+                return (this.testCommand) ?? (this.testCommand = new DelegateCommand(ActionDoit));
+            }
+        }
+
+        private void ActionDoit()
+        {
+            Number++;
+        }
+       
+    }
+
+    #region DelegateCommand Class
+    public class DelegateCommand : ICommand
+    {
+
+        private readonly Func<bool> canExecute;
+        private readonly Action execute;
+
+        /// <summary>
+        /// Initializes a new instance of the DelegateCommand class.
+        /// </summary>
+        /// <param name="execute">indicate an execute function</param>
+        public DelegateCommand(Action execute) : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the DelegateCommand class.
+        /// </summary>
+        /// <param name="execute">execute function </param>
+        /// <param name="canExecute">can execute function</param>
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        /// <summary>
+        /// can executes event handler
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// implement of icommand can execute method
+        /// </summary>
+        /// <param name="o">parameter by default of icomand interface</param>
+        /// <returns>can execute or not</returns>
+        public bool CanExecute(object o)
+        {
+            if (this.canExecute == null)
+            {
+                return true;
+            }
+            return this.canExecute();
+        }
+
+        /// <summary>
+        /// implement of icommand interface execute method
+        /// </summary>
+        /// <param name="o">parameter by default of icomand interface</param>
+        public void Execute(object o)
+        {
+            this.execute();
+        }
+
+        /// <summary>
+        /// raise ca excute changed when property changed
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            if (this.CanExecuteChanged != null)
+            {
+                this.CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+    #endregion
+}
